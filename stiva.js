e01a5da04aa6242c9f33610a93371e63fff4f8d6
Luce Studio <coltpini@luce.studio>
2019-04-04 17:29:50 -0400

    fixed the handler type error, and added a detach function.

--- stiva.js
+++ stiva.js
@@ -1,24 +1,29 @@
 class Stiva {
-    constructor(stores = {}) {
-      this.stores = stores;
-      this.context = new EventTarget();
-    }
-    update(type, store) {
-      let newStore = store(this.stores[type]);
-      this.stores[type] = newStore;
-      this.dispatch(type);
-    }
-    dispatch(type) {
-      this.context.dispatchEvent(
-        new CustomEvent(`stiva-${type}`, {
-          detail: this.stores[type]
-        })
-      );
-    }
-    dispatchAll() {
-      Object.keys(this.stores).forEach(store => this.dispatch(store));
-    }
-    listen(type, handler) {
-      this.context.addEventListener(type, handler);
-    }
+  constructor(stores = {}) {
+    this.stores = stores;
+    this.context = new EventTarget();
+  }
+  update(type, store) {
+    let newStore = store(this.stores[type]);
+    this.stores[type] = newStore;
+    this.dispatch(type);
+  }
+  dispatch(type) {
+    this.context.dispatchEvent(
+      new CustomEvent(`stiva-${type}`, {
+        detail: this.stores[type]
+      })
+    );
+  }
+  dispatchAll() {
+    Object.keys(this.stores).forEach(store => this.dispatch(store));
+  }
+  listen(type, handler) {
+    const h = ({detail}) => handler(detail);
+    this.context.addEventListener(`stiva-${type}`, h);
+    return h;
+  }
+  detach(type, listener) {
+    this.context.removeEventListener(`stiva-${type}`, listener);
+  }
 }
